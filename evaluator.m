e4c4fac639ac7322a62bbf3c2a3711ab4bd04ee9
Marco <marco@MacBook-Pro-de-Marco.local>
2018-08-25 17:23:09 -0500

    Final tests.

--- evaluator.m
+++ evaluator.m
@@ -5,22 +5,28 @@
 
 % ssimval = ssim(A,ref)
 
-A = imread('Weight Mode Filter.png');
-ref = imread('teddy2_manual3.png');
+% A = imread('filtered/venus-6/disp6-venus-noise-MCbR.png');
+% A = imread('filtered/venus-6/Filtered_average6-venus4.png');
+% A = imread('filtered/venus-6/Anisotropic Diffusion.png');
+% A = imread('filtered/venus-6/Bilateral Filter.png');
+% A = imread('filtered/venus-6/Bilateral Upsampling.png');
+% A = imread('filtered/venus-6/Layered Bilateral Filter.png');
+% A = imread('filtered/venus-6/Markov Random Field(Kernel Data Term).png');
+% A = imread('filtered/venus-6/Markov Random Field(Tensor).png');
+% A = imread('filtered/venus-6/Noise-aware Filter.png');
+A = imread('filtered/venus-6/Weight Mode Filter.png');
 
-ssimval = ssim(A,ref(:,:,1))
+ref = imread('filtered/venus-6/disp6-venus.pgm');
+
+ssimval = ssim(A(:,:,1),ref(:,:,1))
 % ssimval = ssim(A, ref)
 
 % psnr
 % Peak Signal-to-Noise Ratio (PSNR)
-
 % peaksnr = psnr(A,ref)
-
-peaksnr = psnr(A,ref(:,:,1))
+peaksnr = psnr(A(:,:,1),ref(:,:,1))
 
 % mse
 % Mean-squared error
-
 % err = immse(A, ref)
-
-err = immse(A,ref(:,:,1))
+err = immse(A(:,:,1),ref(:,:,1))
